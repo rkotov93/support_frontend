80465aa0adaacb75ae1adfc990cd39930508f5bc
Ruslan Kotov <rkotov93@gmail.com>
2017-02-01 22:35:55 +0200

    Refresh user when enter the site

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,12 +1,27 @@
 import { browserHistory } from 'react-router'
 import { AUTH } from '../constants/sessions'
+import * as constants from '../constants/sessions'
 import { loginSuccess } from '../actions/SessionActions'
+import headers from './headers'
 
 export const appInitialize = (dispatch) => {
   return () => {
     const auth = localStorage.getItem(AUTH)
     if (auth)
-      dispatch(loginSuccess(JSON.parse(auth)))
+      fetch(`${process.env.API_HOST}/api/v1/users/me.json`, {
+        headers: headers()
+      }).then(response => {
+        return response.json().then(json => {
+          return { json, response }
+        })
+      }).then(({ json, response }) => {
+        if (response.ok) {
+          localStorage.setItem(constants.AUTH, JSON.stringify(json.user))
+          dispatch(loginSuccess(json.user))
+        }
+        else
+          browserHistory.push('/login')
+      }).catch(() => browserHistory.push('/login'))
     else
       browserHistory.push('/login')
   }
